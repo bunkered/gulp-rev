fd4bd69a17733a018ebd1bf2d32ff180c19af89c
Simon Delaney <simon.delaney.uk@gmail.com>
2016-11-21 20:52:48 +0000

    Add cdn option to revision manifest to allow absolute URL.

--- index.js
+++ index.js
@@ -113,7 +113,8 @@ plugin.manifest = (pth, opts) => {
 	opts = Object.assign({
 		path: 'rev-manifest.json',
 		merge: false,
-		transformer: JSON
+		transformer: JSON,
+		cdn: ''
 	}, opts, pth);
 
 	let manifest = {};
@@ -128,7 +129,7 @@ plugin.manifest = (pth, opts) => {
 		const revisionedFile = relPath(file.base, file.path);
 		const originalFile = path.join(path.dirname(revisionedFile), path.basename(file.revOrigPath)).replace(/\\/g, '/');
 
-		manifest[originalFile] = revisionedFile;
+		manifest[originalFile] = opts.cdn + revisionedFile;
 
 		cb();
 	}, function (cb) {
